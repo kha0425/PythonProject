5f0c055665bc32d5ee56ea7fb0e6aef20c31a042
kimhyuna <hyunkr332@gmail.com>
2024-10-30 17:04:18 +0900

    PythonProject upload

---
+++ share.py
@@ -0,0 +1,7 @@
+openweathermap_api_key = '3bb70457d4ff6e43c946bd69fe902a6c'
+data_service_key_dicode = 'bU7bkB9T3Ni4P9Mhnxg+06F7iITrULBZNgf+v2KpJ8fd+XzmkSWBRrwSNeOE54Qo2NOPlTL2OQuFGhOVG3adAw=='
+# 다나와 로그인 정보 함수
+def get_danawa_login_info():
+    my_id = 'asdf0810'
+    my_pass = 'Ksj1402!'
+    return my_id, my_pass
